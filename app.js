4fdd1494dbd95bd46a9dfc67853f8e01b8779bea
StephB-97 <stephaniebstephy@gmail.com>
2026-03-14 11:38:38 -0400

    Fix session cookie config and navbar currentUser

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ const sessionConfig ={
     secret: 'thisshouldbeabettersecret',
     resave: false,
     saveUninitialized: true,
-    expires: {
+    cookie: {
         httpOnly: true,
         expires: Date.now()+ 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
